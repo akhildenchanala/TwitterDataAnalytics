2b12db3ab234dc0e928beef7c3a766124e7f17c5
Akhil Denchanala <akhil.denchanala@gmail.com>
2018-12-27 12:32:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ def index():
 	if request.method == "GET":
 		search = "2017"
 		(a,b,c,d)=Twitter(search)
-		print(a,b,c,d)
 		return render_template('home.html',tweet_map=a,retweet_table=(b),trend_line=(c),hashtags = (d),search=search)
 	else:	
 
 		search = request.form["select"]
 		print(search)
 		(a,b,c,d)=Twitter(search)
-		print(a,b,c,d)
 		return render_template('home.html',tweet_map=a,retweet_table=(b),trend_line=(c),hashtags = (d),search=search)
 	return "<h1>Something went wrong !! </h1>"
 
